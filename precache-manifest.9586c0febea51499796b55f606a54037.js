732cc9ee6ae4d006254b1be3c451b5f376667154
Montree Puttan <montree@mintel.tech>
2019-11-06 11:42:24 +0700

    Updates

---
+++ precache-manifest.9586c0febea51499796b55f606a54037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef116032556c2a420a46383df6612ef",
+    "url": "/react-gh-page/index.html"
+  },
+  {
+    "revision": "95586b581b19c3209915",
+    "url": "/react-gh-page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2e13ae3758f6cd3b22d3",
+    "url": "/react-gh-page/static/js/2.adbac117.chunk.js"
+  },
+  {
+    "revision": "95586b581b19c3209915",
+    "url": "/react-gh-page/static/js/main.3924cedf.chunk.js"
+  },
+  {
+    "revision": "f217dad316def823ccc0",
+    "url": "/react-gh-page/static/js/runtime-main.e9796264.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-page/static/media/logo.25bf045c.svg"
+  }
+]);
